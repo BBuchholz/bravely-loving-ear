d792a0b06046f8ff17ae7909f04d1d0007e0de19
Brent Buchholz <brentbuchholz@gmail.com>
2023-05-04 15:14:14 -0500

    Added editor under channard myrki

---
+++ src/pages/hi/channard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+import MdEditor from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
+
+const textContent = `
+The mind is a labyrinth, ladies and gentlemen, a puzzle. And while the paths of the brain are plainly visible, its ways deceptively apparent, its destinations are unknown. Its secrets still secret, and, if we are honest, it is the lure of the labyrinth that draws us to our chosen field. To unlock those secrets. Others have been here before us and have left us signs, but we, as explorers of the mind, must devote our lives and energies to going further. To tread the unknown corridors in order to find ultimately, the final solution. We have to see. We have to know.
+
+- Dr. Philip Channard, Hellbound: Hellraiser II
+`
+
+const text = ref(textContent)
+</script>
+
+<template>
+  <MdEditor v-model="text" language="en-US" />
+</template>
